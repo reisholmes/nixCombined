e4a7fdef6f07aad57df444cec5b91cc327d3c7c2
reisholmes <reisholmes@gmail.com>
2025-04-27 20:56:13 +0200

    updated home manager rh-sb3

--- home/reis/rh-sb3/default.nix
+++ home/reis/rh-sb3/default.nix
@@ -2,4 +2,10 @@
   imports = [
     "${nhModules}/common"
   ];
+
+  # Enable home-manager
+  programs.home-manager.enable = true;
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.11";
 }
